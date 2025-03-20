11b31387535a3b9cd4ac60b4e044186339d7459f
yjkim0526 <koiforever0526@gmail.com>
2025-03-20 11:34:43 +0900

    memo edit

--- components/todo_reduce/Header.jsx
+++ components/todo_reduce/Header.jsx
@@ -3,7 +3,6 @@ import React from "react";
 function Header() {
   return (
     <div className="text-xl flex flex-col gap-5">
-      <h2>( useReduce / localStorage save )</h2>
       <h3>오늘은📆</h3>
       <h1>{new Date().toDateString()}</h1>
     </div>
